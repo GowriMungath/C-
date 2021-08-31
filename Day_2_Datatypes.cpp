8491b6ce56047761bc842f242f8db1399fa7ef85
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-08-31 19:12:21 +0530

    Create Day_2_Datatypes.cpp

---
+++ Day_2_Datatypes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=5;
+    float f=8.9;
+    char ch='c';
+    bool b=0;
+    cout<<a<<" is an Integer with size "<<sizeof(int)<<" bytes. "<<endl;
+    cout<<f<<" is a floating point value with size "<<sizeof(float)<<" bytes. "<<endl;
+    cout<<ch<<" is a character with size "<<sizeof(char)<<" byte. "<<endl;
+    cout<<b<<" is a boolean value with size "<<sizeof(bool)<<" byte. "<<endl;
+}
