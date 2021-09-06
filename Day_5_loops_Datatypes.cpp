a901ef8adba9591b88a9d65c14331cdaa575d38e
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-06 19:34:52 +0530

    Update Day_5_loops_Datatypes.cpp

--- Day_5_loops_Datatypes.cpp
+++ Day_5_loops_Datatypes.cpp
@@ -29,3 +29,22 @@ int main()
     while(q>=0);
 }
 
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int a=5;
+    char ch='c';
+    bool b=true;
+    float f=4.5;
+    double d=7.97;
+    string str="Gowri";
+    float scientific=4e3;
+    double sci=8E4;
+    char p=75, c=45;
+    cout<<a<<" "<<ch<<" "<<b<<" "<<f<<" "<<d<<" "<<str<<endl;
+    cout<<scientific<<" "<<sci<<" "<<endl;
+    cout<<p<<" "<<c;
+}
