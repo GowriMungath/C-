a52226253a88d87586dd8aea2e7e049c8d539ebb
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-09 16:59:56 +0530

    Update Day_7_Switch.cpp

--- Day_7_Switch.cpp
+++ Day_7_Switch.cpp
@@ -55,5 +55,30 @@ int main()
         cout<<"Invalid Entry!";
         break;
     }
-    
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    string a="Gowri";
+    string b="Mungath";
+    cout<<a+" "+b<<endl;
+    cout<<a.append(b)<<endl;
+    int c=5;
+    int d=7;
+    cout<<c+d<<endl;
+    //cout<<a+c; error
+    cout<<a.length()<<endl;
+    cout<<b.size()<<endl;
+    cout<<a[2]<<endl;
+    b[3]='p';
+    cout<<b<<endl;
+    string e,f;
+    //cout<<"Enter your brother's full name: ";
+    //cin>>e;
+    //cout<<e<<endl;
+    cout<<"Again enter and now see: ";
+    getline(cin,f);
+    cout<<f<<endl;
 }
