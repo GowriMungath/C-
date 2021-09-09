f9f8579461ce7e8c4ab13392856ffb59ea2d5694
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-09 16:46:47 +0530

    Update Day_7_Switch.cpp

--- Day_7_Switch.cpp
+++ Day_7_Switch.cpp
@@ -26,3 +26,34 @@ int main()
     }
     
 }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,ch;
+    cout<<"1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n";
+    cout<<"Enter the operands: ";
+    cin>>a>>b;
+    cout<<"Enter your choice: ";
+    cin>>ch;
+    switch (ch)
+    {
+    case 1:
+        cout<<a+b;
+        break;
+    case 2:
+        cout<<a-b;
+        break;
+    case 3:
+        cout<<a*b;
+        break;
+    case 4:
+        cout<<a/float(b);
+        break;
+    default:
+        cout<<"Invalid Entry!";
+        break;
+    }
+    
+}
