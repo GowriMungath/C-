2bf3ea31150e2f9b96f533d91b5c8cfe822b149e
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-10 16:07:42 +0530

    Update Day_8_operators.cpp

--- Day_8_operators.cpp
+++ Day_8_operators.cpp
@@ -31,3 +31,17 @@ int main()
     int a=10;
     a%2==0?cout<<"even":cout<<"odd";
 }
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int a=10;
+    int b=9;
+    cout<<max(a,b)<<endl;
+    cout<<min(a,b)<<endl;
+    cout<<sqrt(b)<<endl;
+    cout<<round(10/3)<<endl;
+    cout<<log(10)<<endl;
+}
