0356451a3f5082b3dcd086874773498bd8313610
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-11 12:49:16 +0530

    Create Day_9_bool.cpp

---
+++ Day_9_bool.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    bool a=true;
+    cout<<a<<endl;
+    int b=10;
+    int c=20;
+    cout<<(b==c)<<endl;
+    cout<<(b>c)<<endl;
+    cout<<(b<c);
+}
